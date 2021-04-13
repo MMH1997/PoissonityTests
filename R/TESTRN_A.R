f45dae6da51c9e4d5d7c49aea777ca5eee72df60
MMH1997 <72887498+MMH1997@users.noreply.github.com>
2021-04-13 19:20:38 +0200

    Update TESTRN_A.R

--- R/TESTRN_A.R
+++ R/TESTRN_A.R
@@ -4,7 +4,9 @@
 #' @param n.boot numeric variable
 
 testRna = function(x,a,n.boot){
-  Rna = function(z,c){
+
+testRna1 = function(x,a,n.boot){
+  Rna1 = function(z,c){
     Gna = function(t,y,b){
       (sqrt(length(y))*((1/length(y)) * sum(t^y) -
                           exp(mean(y)*(t-1))))^2 * t^b
@@ -16,10 +18,10 @@ testRna = function(x,a,n.boot){
   pv = 0
   n = length(x)
   lambda.hat=mean(x)
-  T.obs=Rna(x,a)
+  T.obs=Rna1(x,a)
   for (i in 1:n.boot){
     x.boot=rpois(n,lambda.hat)
-    T.boot=Rna(x.boot,a)
+    T.boot=Rna1(x.boot,a)
     pv=pv+as.numeric(T.boot>T.obs)
   }
   names(T.obs) <- "test statistic"
@@ -37,42 +39,45 @@ testRna = function(x,a,n.boot){
 
 
 
-# library(pracma)
-#
-# testRna1 = function(x,a,n.boot){
-#   Rna1 = function(z,b){
-#     aux = outer(z,z, "+") + b + 1
-#     t = sum(1/aux)
-#     coc1 = t/length(z)
-#     r = 0
-#     for (i in 1:length(z)) {
-#       r = r + ((exp(-mean(z))/((-mean(z))^(z[i] + b + 1))) * as.numeric(gammainc(a = (z[i] + b + 1), x = (-mean(z)))[1]))
-#     }
-#     coc2 = 2*r
-#     num3 = length(z) * exp(-2*mean(z))
-#     den3 = (-2 * mean(z))^(b + 1)
-#     coc3 = (num3/den3)* as.numeric(gammainc(a = (b + 1), x = (-2*mean(z)))[1])
-#     coc1 - coc2 + coc3
-#   }
-#   pv = 0
-#   n = length(x)
-#   lambda.hat=mean(x)
-#   T.obs=Rna1(x,a)
-#   for (i in 1:n.boot){
-#     x.boot=rpois(n,lambda.hat)
-#     T.boot=Rna1(x.boot,a)
-#     pv=pv+as.numeric(T.boot>T.obs)
-#   }
-#   names(T.obs) <- "test statistic"
-#   media = mean(x)
-#   names(media) <- "mean"
-#   e = list(method = paste("Rn,a test",
-#                           sep = ""),
-#            statistic = T.obs, p.value = pv/n.boot,
-#            data.name = paste("sample size ", n, ", replicates ",
-#                              n.boot, sep = ""), estimate = media)
-#   class(e) <- "htest"
-#   e
-# }
 
+testRna2 = function(x,a,n.boot){
+  Rna2 = function(z,b){
+    aux = outer(z,z, "+") + b + 1
+    t = sum(1/aux)
+    coc1 = t/length(z)
+    r = 0
+    for (i in 1:length(z)) {
+      r = r + ((exp(-mean(z))/((-mean(z))^(z[i] + b + 1))) * as.numeric(gammainc(a = (z[i] + b + 1), x = (-mean(z)))[1]))
+    }
+    coc2 = 2*r
+    num3 = length(z) * exp(-2*mean(z))
+    den3 = (-2 * mean(z))^(b + 1)
+    coc3 = (num3/den3)* as.numeric(gammainc(a = (b + 1), x = (-2*mean(z)))[1])
+    coc1 - coc2 + coc3
+  }
+  pv = 0
+  n = length(x)
+  lambda.hat=mean(x)
+  T.obs=Rna2(x,a)
+  for (i in 1:n.boot){
+    x.boot=rpois(n,lambda.hat)
+    T.boot=Rna2(x.boot,a)
+    pv=pv+as.numeric(T.boot>T.obs)
+  }
+  names(T.obs) <- "test statistic"
+  media = mean(x)
+  names(media) <- "mean"
+  e = list(method = paste("Rn,a poissonity test",
+                          sep = ""),
+           statistic = T.obs, p.value = pv/n.boot,
+           data.name = paste("sample size ", n, ", replicates ",
+                             n.boot, sep = ""), estimate = media)
+  class(e) <- "htest"
+  e
+}
 
+if (length(x) > 10) {
+  return(testRna1(x,a,n.boot))
+}
+else {return(testRna2(x,a,n.boot))}
+}
